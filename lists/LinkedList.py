2241f5b4e2b69f499363365d5d87ce428dfaf5fb
Ayush Arora <ayusharora009@gmail.com>
2020-08-10 21:15:33 -0700

    Update LinkedList.py

--- lists/LinkedList.py
+++ lists/LinkedList.py
@@ -1,3 +1,14 @@
+# all nodes in the linked list are 0-indexed
+# the head node represents the entire linked list
+
+# Functions:
+
+# get(index)
+# addAtHead(val)
+# addAtTail(val)
+# addAtIndex(index, val)
+# deleteAtIndex(index)
+
 from . import Node
 
 class LinkedList:
