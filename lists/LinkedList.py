d3cdd903137a160d05c97bb14ae1122dee201eb0
Ayush Arora <ayusharora009@gmail.com>
2020-08-10 21:16:54 -0700

    Update LinkedList.py

--- lists/LinkedList.py
+++ lists/LinkedList.py
@@ -3,11 +3,11 @@
 
 # Functions:
 
-# get(index)
-# addAtHead(val)
-# addAtTail(val)
-# addAtIndex(index, val)
-# deleteAtIndex(index)
+# get(index) : Get the value of the index-th node in the linked list. If the index is invalid, return -1.
+# addAtHead(val) : Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
+# addAtTail(val) : Append a node of value val to the last element of the linked list.
+# addAtIndex(index, val) : Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
+# deleteAtIndex(index) : Delete the index-th node in the linked list, if the index is valid.
 
 from . import Node
 
